18212aff20291ed19b2ab2acf8c4d362e3d34bc9
s bacha <sam@manifoldfinance.com>
2022-09-18 00:39:22 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 echo "Well Known Dir Setup init"
+
+
+mkdir -p .well-known/
+cd .well-known/
+
+sleep 1
+echo "Well Know Dir Init!"
+
 mkdir -p apple-app-site-association/
 touch apple-app-site-association/.gitkeep
+sleep 1
+
 mkdir -p security/
 touch security/.gitkeep
+sleep 1
+
 mkdir -p canary/
 toch canary/.gitkeep
+
 sleep 1
 echo "setup finished..."
